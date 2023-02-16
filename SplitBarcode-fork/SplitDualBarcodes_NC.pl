b6471f77dab5cb0e857ea92af5a6f1e3bc22a11f
Stephane Plaisance <stephane.plaisance@vib.be>
2023-02-16 13:50:50 +0100

    Update SplitDualBarcodes_NC.pl
    new version using Text::Levenshtein::Flexible

--- SplitBarcode-fork/SplitDualBarcodes_NC.pl
+++ SplitBarcode-fork/SplitDualBarcodes_NC.pl
@@ -6,8 +6,12 @@ use Cwd 'abs_path';
 use File::Basename;
 use Data::Dumper;
 use FileHandle;
+# add edit distance function
+use Text::Levenshtein::Flexible qw( levenshtein levenshtein_l_all );
 
-# Stéphane Plaisance - VIB-Nucleomics Core - 2023-02-10 v1.00
+# script: SplitDualBarcodes_NC2.pl
+# rewritten with edit distance for correction
+# Stéphane Plaisance - VIB-Nucleomics Core - 2023-02-14 v1.04
 # visit our Git: https://github.com/Nucleomics-VIB
 #
 # this script runs a modified version of the original 'SplitDualBarcodes.pl'
@@ -17,10 +21,10 @@ use FileHandle;
 #
 # + small edits:
 # -+ replace gzip post-compression through shell scripts by gzip in-pipe compression
-# + handle undef $correct{$barhash{$barseq}} and $correctedBar{$barhash{$barseq}}
 # -+ changed headers and file names to match current on-device output
+# Text::Levenshtein to correct barcodes
 
-my $version="1.01, 2023_02_14";
+my $version="1.04, 2023_02_16";
 
 my $usage=<<USAGE;
 	Usage:
@@ -28,22 +32,23 @@ my $usage=<<USAGE;
 			*-r1 --read1 <string>		read1.fq.gz
 			*-r2 --read2 <string>		read2.fq.gz
 			 -e  --errNum <int>		mismatch number for each barcode/index [default: 1]
-			*-f  --firstCycle <int>		first cylce of barcode
-			*-b  --barcodeList <string>	barcodes list
-			 -rc --revcom	<Y|N>		generate reverse complement of barcode.list or not
+			*-f  --firstCycle <int>		first cycle of barcode
+			*-b  --barcodeList <string>	barcodes list (tab or space-delimited)
+			 -rc --revcom	<Y|N>		generate reverse complement of barcode.list [default: N]
 			 -c  --compress <Y|N>		compress (.gz) output or not [default: Y]
 			 -o  --outdir <string>		output directory [default: ./]
 			 -h  --help			print help information and exit
 	Example:
 		perl $0 -r1 read1.fq.gz -r2 read2.fq.gz -e 2 -f 101 -b barcode.list -o /path/outdir
 		
-	============barcode.list===========
-	#barcodeNum	barcodeSeq1	barcodeSeq2
-	1	ATGCATCTAA	TATAGCCTAG
-	2	AGCTCTGGAC	CTCTATCGTC
-	===================================
+	=======example barcode.list================
+	#barcodeName	barcodeSeq1	barcodeSeq2
+	barcode_1	ATGCATCTAA	TATAGCCTAG
+	barcode_2	AGCTCTGGAC	CTCTATCGTC
+	===========================================
 	
-	REM: VIB-NC version $version adapted from the original gateswell's SplitDualBarcodes.pl
+	(adapted from the original gateswell's SplitDualBarcodes.pl)
+	VIB-NC version $version
 USAGE
 
 #=============global variants=============
@@ -61,6 +66,8 @@ GetOptions(
 	"outdir|o:s"=>\$outdir,
 	"help|h:s"=>\$help
 );
+
+# default errNum if not provided
 #$errNum ||= 1;
 if(defined $errNum && $errNum == 0){
 	$errNum=0;
@@ -68,16 +75,18 @@ if(defined $errNum && $errNum == 0){
 elsif(!defined $errNum ){
 	$errNum ||= 1;
 }
+
 $outdir ||= `pwd`;
 $compress ||= 'Y';
-$rc ||= 'Y';
+$rc ||= 'N';
 
+# required arguments or die
 if(!$read1 || !$read2 || !$fc || !$bl || $help ){
 	die "$usage";
 }
 
 #========global variables==========
-my (%barhash,%oh,%oribar,%correctBar,%correctedBar,%unknownBar,$totalReadsNum);
+my (%barhash,%oh,%oribar,%oriname,%correctBar,%correctedBar,%unknownBar,$totalReadsNum);
 my (%tagNum,$am1,$am2,@fq,$barcode_len,$barcode_len1,$barcode_len2,%ori_tag,%mis_barNum);
 #=========================
 
@@ -92,7 +101,10 @@ unless(-d $outdir){
 }
 $outdir=abs_path($outdir);
 chomp($outdir);
-open my $fh,$bl or die "$bl No such file, check it !\n$!";
+
+###################################
+# create files for undecoded reads
+###################################
 
 # create undecoded fastq output file handles
 if(uc($compress) eq 'Y'){
@@ -106,17 +118,15 @@ if(uc($compress) eq 'Y'){
 push @fq,"$outdir/$prefix\_undecoded_1.fq";
 push @fq,"$outdir/$prefix\_undecoded_2.fq";
 
-# create summary files 
-open my $BS,">$outdir/BarcodeStat.txt" or die $!;
-open my $SS,">$outdir/SequenceStat.txt" or die $!;
+########################################
+# load barcode file and store in arrays
+########################################
 
-# add header to summary files
-#print $BS "#SpeciesNO\tCorrect\tCorrected\tTotal\tPct\n";
-#print $SS "#Sequence\tSpeciesNO\treadCount\tPct\n";
+# barcode file handle
+open my $fh,$bl or die "$bl barcode list not found !\n$!";
 
-# changed header names to match current on-device output
-print $BS "#Barcode\tCorrect\tCorrected\tTotal\tPercentage(%)\n";
-print $SS "#Sequence\tBarcode\tCount\tPercentage(%)\n";
+# store barcodes and sample names for use by tryCorrect function
+my (@name,@correct,@bc1,@bc2);
 
 # loop through all barcode pairs from provided list
 while(<$fh>){	#1	ATGCATCTAA	TATAGCCTAG
@@ -127,53 +137,74 @@ while(<$fh>){	#1	ATGCATCTAA	TATAGCCTAG
     # if option rc was set to 'Y'
 	if(uc($rc) eq 'Y'){
         # reverse the barcodes
-		$tmp[1]=reverse(uc($tmp[1]));	#AATCTACGTA
-		$tmp[2]=reverse(uc($tmp[2]));	#GATCCGATAT
-		$barcode_seq=$tmp[2].$tmp[1];	#GATCCGATATAATCTACGTA
+		my $t=reverse(uc($tmp[1]));		#AATCTACGTA
+		$tmp[1]=reverse(uc($tmp[2]));	#GATCCGATAT
+		$tmp[2]=$t;
+		$barcode_seq=$tmp[1].$tmp[2];	#GATCCGATATAATCTACGTA
         # complement the merged sequence
 		$barcode_seq=~tr/ATCGN/TAGCN/;	#CTAGGCTATATTAGATGCAT
 	}else{
+	    # barcodes are read as-is
 		$tmp[1]=uc($tmp[1]);			#ATGCATCTAA
 		$tmp[2]=uc($tmp[2]);			#TATAGCCTAG
 		$barcode_seq=$tmp[1].$tmp[2];	#ATGCATCTAATATAGCCTAG
 	}
-	$oribar{$barcode_seq} = 1;
-	$barcode_len1=length($tmp[1]);$barcode_len2=length($tmp[2]);
-	&bar_hash($barcode_seq,$tmp[0],$errNum,\%barhash);
 	
-	# create demultiplexed fastq output file handles
+	# add $barcode_name to array of original barcodes
+	push(@name,$tmp[0]);
+	
+	# add barcode name to hash with barcode_seq as key
+	$oriname{$barcode_seq} = $tmp[0];	
+	
+	# add both $barcode sequences to array of original barcodes
+	push(@bc1,$tmp[1]);
+	push(@bc2,$tmp[2]);
+
+	# add merged to correct barcodes
+	push(@correct,$barcode_seq);
+
+	# add merged barcode to hash of original barcodes
+	$oribar{$barcode_seq} = 1;
+	
+	# create demultiplexed fastq output file handles for both reads
 	if(uc($compress) eq 'Y'){
- 	    open $oh{$barhash{$barcode_seq}}[0],"|gzip -9 >$outdir/$prefix\_$tmp[0]\_1.fq.gz" or die $!;
-	    open $oh{$barhash{$barcode_seq}}[1],"|gzip -9 >$outdir/$prefix\_$tmp[0]\_2.fq.gz" or die $!;
+ 	    open $oh{$tmp[0]}[0],"|gzip -9 >$outdir/$prefix\_$tmp[0]\_1.fq.gz" or die $!;
+	    open $oh{$tmp[0]}[1],"|gzip -9 >$outdir/$prefix\_$tmp[0]\_2.fq.gz" or die $!;
     }else{
-        open $oh{$barhash{$barcode_seq}}[0],">$outdir/$prefix\_$tmp[0]\_1.fq" or die $!;
-	    open $oh{$barhash{$barcode_seq}}[1],">$outdir/$prefix\_$tmp[0]\_2.fq" or die $!;
+        open $oh{$tmp[0]}[0],">$outdir/$prefix\_$tmp[0]\_1.fq" or die $!;
+	    open $oh{$tmp[0]}[1],">$outdir/$prefix\_$tmp[0]\_2.fq" or die $!;
     }
-    # add both demultiplexed read filenames to fastq-name array
+    # add both demultiplexed read filenames to fastq-name array @fq
 	push @fq,"$outdir/$prefix\_$tmp[0]\_1.fq";
 	push @fq,"$outdir/$prefix\_$tmp[0]\_2.fq";
 }
 close $fh;
 
+# measure both barcode lengths from first barcode pair
+$barcode_len1=length($bc1[0]);
+$barcode_len2=length($bc2[0]);
 $barcode_len=$barcode_len1+$barcode_len2;
-for my $line_seq(keys %barhash){
-		$mis_barNum{$line_seq} ++;
-		if ($mis_barNum{$line_seq} >=2){
-			print STDERR "Set a smaller mismatch value because of same barcodes\($barhash{$line_seq}\t$line_seq\) are same!\n";
-			exit;
-			}
-}
 
-# read from paired raw fastq file
+###############################################
+# read from paired raw fastq file and classify
+###############################################
+
 my($rd1,$rd2);
+
+# check if read2 is fastq or fastq.gz
 if($read2=~/fq$/){
 	open $rd1,$read1 or die $!;
 	open $rd2,$read2 or die $!;
 }elsif($read2=~/fq.gz$/){
 	open $rd1,"zcat $read1|" or die $!;
-}   open $rd2,"zcat $read2|" or die $!;
+    open $rd2,"zcat $read2|" or die $!;
+}else {
+	die "read2 file does not have the expected extension (.fq or .fq.gz)!\n";
+}
 
 while(<$rd1>){
+
+    # read 4 lines from both reads and store them to fastq fields
 	my $head1= $_;
 	my $seq1 = <$rd1>;
 	my $plus1= <$rd1>;
@@ -182,100 +213,168 @@ while(<$rd1>){
 	my $seq2 = <$rd2>;
 	my $plus2= <$rd2>;
 	my $qual2= <$rd2>;
-	$totalReadsNum ++;
+	
+	# chomp all strings
 	chomp($head1,$seq1,$plus1,$qual1,$head2,$seq2,$plus2,$qual2);
+
+	# count read pair
+	$totalReadsNum ++;
+	#$totalReadsNum =~ m/00000$/ and print STDERR "$totalReadsNum\n";
+    #$totalReadsNum =~ m/00000$/ and last;
+	
+	# extract double-barcode sequence from read2 at expected starting position $fc (-f)
 	my $barseq=substr($seq2,$fc-1,$barcode_len+1);
+	#print STDOUT "# $barseq\n";
+	
+	# add 1 to this barcode counter for SequenceStat.txt
+	# note: can present missmatches with the expected barcode
 	$tagNum{$barseq} ++;
-	$ori_tag{$barseq} = substr($seq2,$fc-1,$barcode_len1+1)."-".substr($seq2,$fc+$barcode_len1-1,$barcode_len2+1);
-	if(exists $barhash{$barseq}){
-	    # trim barcode from seq and qual lines of read2
-		my $spitseq2=substr($seq2,0,$fc-1).substr($seq2,$fc+$barcode_len-1,);
-		my $spitqual2=substr($qual2,0,$fc-1).substr($qual2,$fc+$barcode_len-1,);
-		$oh{$barhash{$barseq}}[0]->print("$head1\n$seq1\n$plus1\n$qual1\n");
-		$oh{$barhash{$barseq}}[1]->print("$head2\n$spitseq2\n$plus2\n$spitqual2\n");
-		# classify barcode type
-		if(exists $oribar{$barseq}){
-			$correctBar{$barhash{$barseq}} +=1;
-		}else{
-			$correctedBar{$barhash{$barseq}} +=1;
+
+    # clip barcode sequence from read2 seq and qual lines
+	my $clipseq2=substr($seq2,0,$fc-1); #.substr($seq2,$fc+$barcode_len-1,);
+	my $clipqual2=substr($qual2,0,$fc-1); #.substr($qual2,$fc+$barcode_len-1,);	
+	
+	# add merged barcode sequence to read1 name
+	$head1 = $head1." ".$barseq;
+	$head2 = $head2." ".$barseq;
+	
+	#######################################
+	# barcode is found in the allowed list
+	#######################################
+	
+	if(exists $oribar{$barseq}){
+		# add 1 to counter
+    	$correctBar{$barseq} += 1;
+   	
+    	#print STDOUT "# $barseq is found in list with name $oriname{$barseq}\n";
+       	
+   		# write reads to files
+    	$oh{$oriname{$barseq}}[0]->print("$head1\n$seq1\n$plus1\n$qual1\n");
+		$oh{$oriname{$barseq}}[1]->print("$head2\n$clipseq2\n$plus2\n$clipqual2\n");
+		# process next read pair
+		next;
 		}
-	}else{
-		my $spitseq2=substr($seq2,0,$fc-1).substr($seq2,$fc+$barcode_len-1,);
-		my $spitqual2=substr($qual2,0,$fc-1).substr($qual2,$fc+$barcode_len-1,);
+
+	#########################################################
+	# try find a correction with at most $errNum per barcode
+	#########################################################
+	
+	# extract both barcodes for correction testing
+	my $test1 = substr($seq2,$fc-1,$barcode_len1+1);
+	my $test2 = substr($seq2,$fc+$barcode_len1-1,$barcode_len2+1);
+	
+   	my $cor = tryCorrect2($test1,$test2);
+   	if ($cor ne "na"){
+		# add 1 to counter
+   		$correctedBar{$cor} += 1;
+
+   		#print STDOUT "# $name[$cor] $bc1[$cor] $bc2[$cor]\n";
+
+		# write reads to files
+    	$oh{$oriname{$cor}}[0]->print("$head1\n$seq1\n$plus1\n$qual1\n");
+		$oh{$oriname{$cor}}[1]->print("$head2\n$clipseq2\n$plus2\n$clipqual2\n");
+   	} else {
+		# add 1 to counter
+		$unknownBar{$barseq} += 1;
+
+    	#print STDOUT "# barcode is unexpected, store read pair as undecoded\n";
+    	
+		# write reads to files
 		print $am1 "$head1\n$seq1\n$plus1\n$qual1\n";
-		print $am2 "$head2\n$spitseq2\n$plus2\n$spitqual2\n";
-		$unknownBar{$barseq} +=1;
-	}
-}
+		print $am2 "$head2\n$clipseq2\n$plus2\n$clipqual2\n";
+	   	}
+ 	}
+
+# close file handles
 close $rd1;close $rd2;
 close $am1;close $am2;
 
 ###############################
-# print results to BarcodeStat
+# print summary to BarcodeStat
 ###############################
 
+open my $BS,">$outdir/BarcodeStat.txt" or die $!;
+print $BS "#Barcode\tCorrect\tCorrected\tTotal\tPercentage(%)\n";
+
 my($totalcorrect,$totalcorrected,$totalbarreads,$totalpct);
-for my $seq(sort {$barhash{$a} cmp $barhash{$b}} keys %oribar){
-    # unlikely but when $correctBar{$barhash{$seq}} is unset => set it to 0
-    if (!$correctBar{$barhash{$seq}}) {
-        $correctBar{$barhash{$seq}}=0;
+for my $seq(sort {$oriname{$a} cmp $oriname{$b}} keys %oribar){
+	# unlikely but when $correctBar{$barhash{$seq}} is unset => set it to 0
+    if (!$correctBar{$seq}) {
+        $correctBar{$seq}=0;
     }
     # when $correctedBar{$barhash{$seq}} is unset => set it to 0
-    if (!$correctedBar{$barhash{$seq}}) {
-        $correctedBar{$barhash{$seq}}=0;
+    if (!$correctedBar{$seq}) {
+        $correctedBar{$seq}=0;
     }
-    my $BartotalReads = $correctBar{$barhash{$seq}}+$correctedBar{$barhash{$seq}};
+    my $BartotalReads = $correctBar{$seq}+$correctedBar{$seq};
 	my $pct = ($BartotalReads/$totalReadsNum)*100;
-	$totalcorrect += $correctBar{$barhash{$seq}};
-	$totalcorrected+=$correctedBar{$barhash{$seq}};
+	$totalcorrect += $correctBar{$seq};
+	$totalcorrected += $correctedBar{$seq};
 	$totalbarreads += $BartotalReads;
 	$totalpct += $pct;
-	printf $BS "%s\t%d\t%d\t%d\t%.4f%%\n",$barhash{$seq},$correctBar{$barhash{$seq}},$correctedBar{$barhash{$seq}},$BartotalReads,$pct;
+	printf $BS "%s\t%d\t%d\t%d\t%.4f%%\n",$oriname{$seq},$correctBar{$seq},$correctedBar{$seq},$BartotalReads,$pct;
 }
 printf $BS "Total\t%d\t%d\t%d\t%.4f%%\n",$totalcorrect,$totalcorrected,$totalbarreads,$totalpct;
 close $BS;
 
 ###############################
-# print results to SequenceStat
+# print summary to SequenceStat
 ###############################
+
+open my $SS,">$outdir/SequenceStat.txt" or die $!;
+print $SS "#Sequence\tBarcode\tCount\tPercentage(%)\n";
 
 for my $seq(sort {$tagNum{$b}<=>$tagNum{$a}} keys %tagNum){
 	my $pct=($tagNum{$seq}/$totalReadsNum)*100;
-	if(exists $barhash{$seq}){
-		printf $SS "%s\t%s\t%d\t%.2f%%\n",$ori_tag{$seq},$barhash{$seq},$tagNum{$seq},$pct;
+	if(exists $oriname{$seq}){
+		printf $SS "%s\t%s\t%d\t%.2f%%\n",$seq,$oriname{$seq},$tagNum{$seq},$pct;
 	}
 	else{
-		printf $SS "%s\tundecoded\t%d\t%.2f%%\n",$ori_tag{$seq},$tagNum{$seq},$pct;
+		printf $SS "%s\tundecoded\t%d\t%.2f%%\n",$seq,$tagNum{$seq},$pct;
 	}
 }
 close $SS;
 
-#=============subroutine==================
-sub bar_hash{
-	my ($seq,$name,$errnum,$hash)=@_;
-	my ($tmp_seq1,$tmp_seq2);
-	my @bases=('A','T','C','G','N');
-	if($errnum==0){
-		$hash->{$seq}=$name;
-		return $hash;
-	}else{
-		my $seq1 = substr($seq,0,length($seq)/2);	#barcode 1
-		my $seq2 = substr($seq,length($seq)/2,);	#barcode 2
-		for (my $i=0;$i<length($seq1);$i++){
-			for (my $j=0;$j<length($seq2);$j++){
-				for (my $k=0;$k<@bases;$k++){
-					$tmp_seq1 =substr($seq1,0,$i).$bases[$k].substr($seq1,$i+1,);
-					$tmp_seq2 =substr($seq2,0,$j).$bases[$k].substr($seq2,$j+1,);
-					my $tmp_seqs = $tmp_seq1.$tmp_seq2;
-					if($errnum > 1){
-						&bar_hash($tmp_seqs,$name,$errnum-1,$hash);
-					}
-					else{
-						$hash->{$tmp_seqs}=$name;
-					}
-				}
-			}
-		}
-		return $hash;
-	}
+#================================
+sub search_idx {
+my ($q, @arr) = ( @_ );
+#my @matches = grep { /^$q$/ } @arr; # returns elements
+my @matches = grep { $arr[$_] =~ /^$q$/ } 0..$#arr;  # returns indices
+return @matches;
+}
+
+#================================
+sub tryCorrect2 {
+  my ($test1, $test2) = @_;
+  my (@res1,@res2,$merge,$match);
+  
+  @res1=( map { $_->[0] }
+  sort { $a->[1] <=> $b->[1] }
+  levenshtein_l_all($errNum, $test1, @bc1) );
+     
+  # barcode#1 matches or could be corrected
+  if ( @res1 ){
+    # identify the indices of the first barcode match(es)
+    my @idx1 = map {search_idx( $_, @bc1 )} @res1;
+    
+    # only look at the paired barcode#2 in second list
+    @res2=( map { $_->[0] }
+    sort { $a->[1] <=> $b->[1] }
+    levenshtein_l_all($errNum, $test2, @bc2[@idx1]) );
+
+    # barcode#2 matches or could be corrected
+    if ( @res2 ){
+      # corrected pair exists (not barcode hopping)
+      $merge="@res1"."@res2";
+      if ( $oribar{$merge} ){
+        return $merge;
+      }else{
+      return "na";
+      }
+    } else {
+      return "na";
+    }
+  } else {
+    return "na";
+  }
 }
